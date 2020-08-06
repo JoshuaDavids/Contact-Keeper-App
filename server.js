55f2eb9c7b08bf6612675418426e6d4e55469da3
JoshuaDavids <joshuadavids.jcd@gmail.com>
2020-08-06 13:34:58 +0200

    heroku prep

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -9,14 +9,22 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => 
-  res.json({ msg: 'Welcome to the ContactKeeper API' })
-);
+// app.get('/', (req, res) =>
+//   res.json({ msg: 'Welcome to the ContactKeeper API' })
+// );
 
 // Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contact', require('./routes/contact'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contact", require("./routes/contact"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
